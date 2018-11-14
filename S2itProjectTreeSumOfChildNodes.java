28f324cd91bc2675f999d8539bc82d96ed75c8dc
Alan Teixeira <alanfrnk@gmail.com>
2018-11-13 23:07:13 -0200

    Create S2itProjectTreeSumOfChildNodes.java

---
+++ S2itProjectTreeSumOfChildNodes.java
@@ -0,0 +1,32 @@
+package s2itprojecttree;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ *
+ * @author alanfrank
+ */
+public class S2itProjectTreeSumOfChildNodes {
+    public static int sumOfChildNodes(S2itProjectTreeNode rootNode) {
+        
+	Queue<S2itProjectTreeNode> nodeQueue = new LinkedList<>();
+	nodeQueue.offer(rootNode);
+	
+        int sumOfNodes = 0;
+	while (!nodeQueue.isEmpty()) {
+            
+            S2itProjectTreeNode node = nodeQueue.poll();
+            sumOfNodes += node.data;
+            
+            if (node.left != null) {
+                nodeQueue.offer(node.left);
+            }
+            
+            if (node.right != null) {
+                nodeQueue.offer(node.right);
+            }
+	}
+	return sumOfNodes;
+    }
+}
